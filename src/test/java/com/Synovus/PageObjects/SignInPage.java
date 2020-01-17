226e43a7896ad719fd5159540afdb5e931a5f531
Ramesh <rams4automation@gmail.com>
2020-01-17 19:53:52 +0530

    Added updated

--- src/test/java/com/Synovus/PageObjects/SignInPage.java
+++ src/test/java/com/Synovus/PageObjects/SignInPage.java
@@ -1,25 +1,21 @@
 package com.Synovus.PageObjects;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import com.Synovus.accelerators.ActionDriver;
 
 public class SignInPage {
 	
 	WebDriver driver;
 
-	// By user_name = By.id("LoginDeskTopModePage-UserName");
-	// By Pass_WRD = By.id("LoginDeskTopModePage-password");
-	 //By Loginlink = By.xpath("//*[@id='mainNavbar']/div[1]/div[3]/div/ul/li[4]/div/div/a");
-	 //By PersonalMenu = By.xpath("//*[@id='siteNavbar']/li[3]/a");
-	// By BusinessMenu = By.xpath("//*[@id='siteNavbar']/li[4]/a");
-	 By LoginButton = By.id("LoginDeskTopModePage-Login");
-	 By Settings =By.xpath("//*[@id='navigation']/ul/li[5]/a");
-
+	
+	 @FindBy(id="LoginDeskTopModePage-Login")
+	 WebElement LoginButton;
+	 
+	 @FindBy(xpath="//*[@id='navigation']/ul/li[5]/a")
+	 WebElement Settings; 
 	 
 	 @FindBy(xpath="//*[@id='siteNavbar']/li[3]/a")
 	 WebElement PersonalMenu;
@@ -48,36 +44,31 @@ public class SignInPage {
 	
 	public void clickonPersonalSignInbutton() {
 		  PersonalMenu.click();
-        /* WebElement Signintab=driver.findElement(PersonalMenu);
-         if(Signintab.isDisplayed()){
-        	 Signintab.click();
-         }*/
-	}
+       	}
 	
 	
 	public void clickonBusinessSignInbutton() {
 		BusinessMenu.click();
-		//driver.findElement(BusinessMenu).click();
-       /* WebElement Signintab=driver.findElement(BusinessMenu);
-        if(Signintab.isDisplayed()){
-       	 Signintab.click();
-        }*/
-	}
+	   }
 	
 	public void clickonloginlink(){
 		Loginlink.click();
-	  }
+	   }
 	
 	public void clickonBusinessMenu(){
 		this.clickonBusinessSignInbutton();
-	}
+	    }
 	
 	
 	public void EnterBusinessUserNameandPasswd(String Username,String Password){
 		user_name.sendKeys(Username);
 		Pass_WRD.sendKeys(Password);
-	}
+	    }
 	
+	public void EnterPersonalUserNameandPasswd(String Username,String Password){
+		user_name.sendKeys(Username);
+		Pass_WRD.sendKeys(Password);
+	   }
 	
 	
 	
